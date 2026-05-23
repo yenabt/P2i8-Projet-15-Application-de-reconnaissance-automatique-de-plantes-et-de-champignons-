75439e2756cbd81d0287762864f0ac817ef8b8db
AYDIN Gokce <gokceaydin2803@gmail.com>
2026-05-23 17:01:12 +0200

    a dded detection_couleur_cv.py

---
+++ detection_couleur_cv.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from contour import process_flower_image
+import cv2
+
+NAMED_COLORS = {
+    "Rouge":   [53, 80, 67],
+    "Orange":  [63, 43, 74],
+    "Jaune":   [97, -21, 94],
+    "Vert":    [36, -43, 41],
+    "Bleu":    [32, 79, -107],
+    "Violet":  [29, 58, -36],
+    "Rose":    [66, 64, 17],
+    "Blanc":   [100, 0, 0],
+    "Magenta": [60, 93, -60],
+    "Cyan":    [91, -48, -14],
+}
+
+def segment_flower(fleur_bgr):
+    
+    blurred = cv2.GaussianBlur(fleur_bgr, (31, 31), 0)
+
+    fleur_hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    fleur_S = fleur_hsv[:, :, 1]
+    fleur_V = fleur_hsv[:, :, 2]
+
+    _, thresh_s = cv2.threshold(fleur_S, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, thresh_v = cv2.threshold(fleur_V, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    rough = cv2.bitwise_or(thresh_s, thresh_v)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
+    sure_fg = cv2.erode(rough, kernel, iterations=2)
+    sure_bg = cv2.dilate(rough, kernel, iterations=3)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    _, markers = cv2.connectedComponents(sure_fg)
+    markers += 1
+    markers[unknown == 255] = 0
+
+    markers = cv2.watershed(fleur_bgr, markers)
+
+    flower_label = markers[markers.shape[0] // 2, markers.shape[1] // 2]
+    fg_mask = np.uint8(markers == flower_label) * 255
+
+    kernel_small = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, kernel_small)
+
+    contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    fg_mask = np.zeros_like(fg_mask)
+    cv2.drawContours(fg_mask, contours, -1, 255, thickness=cv2.FILLED)
+
+    return fg_mask
+
+
+def detect_color(fleur_bgr, fg_mask):
+    fleur_lab = cv2.cvtColor(fleur_bgr, cv2.COLOR_BGR2Lab).reshape(-1, 3)
+    fg_pixels = fleur_lab[fg_mask.ravel() == 255].astype(np.float32)
+    fg_pixels[:, 0] = fg_pixels[:, 0] * 100 / 255
+    fg_pixels[:, 1] = fg_pixels[:, 1] - 128
+    fg_pixels[:, 2] = fg_pixels[:, 2] - 128
+
+    mean_lab = fg_pixels.mean(axis=0)
+    return min(NAMED_COLORS, key=lambda c: np.linalg.norm(mean_lab - NAMED_COLORS[c]))
+
+def color_and_image(img):
+    fleur_bgr = cv2.imread(os.path.join(script_dir, 'test4.jpeg'))
+    contour = process_flower_image(fleur_bgr)
+    fleur_rgb = cv2.bilateralFilter(cv2.cvtColor(fleur_bgr, cv2.COLOR_BGR2RGB), 9, 75, 75)
+
+    fg_mask = segment_flower(contour)
+    color = detect_color(contour, fg_mask)
+
+    fleur_no_bg = contour.copy()
+    fleur_no_bg[fg_mask == 0] = 0
+
+    return color, fleur_no_bg
+
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+fleur = 'test4.jpeg'
+color, img = color_and_image(fleur)
+
+print(color)
+
+plt.imshow(img)
+plt.show()
